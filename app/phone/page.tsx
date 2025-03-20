601897f22fa398c346a8c0355ac1c9fbaa71cc55
tenmonster <tenmonster@hotmail.com>
2025-03-20 13:28:41 +0700

    Create Phonepage

---
+++ app/phone/page.tsx
@@ -0,0 +1,18 @@
+import styles from "./phone.module.css"
+
+export default function PhonePage() {
+  return (
+    <div className={styles.page}>
+      <div className={styles.container}>
+        <div className={styles.header}>
+          <p>
+            เชื่อมต่อ
+          </p>
+          <p>ด้วยหมายเลขโทรศัพท์มือถือ</p>
+        </div>
+
+      </div>
+
+    </div>
+  )
+}
