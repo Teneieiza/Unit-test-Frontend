b65bafa04d7471f08170ff8c66f5f02d6cb835bb
tenmonster <tenmonster@hotmail.com>
2025-03-20 17:28:14 +0700

    Add more icon

--- app/icon/icon.tsx
+++ app/icon/icon.tsx
@@ -6,3 +6,11 @@ export const ArrowIcon: React.FC = () => (
     <path d="M20 11H7.83l5.59-5.59L12 4l-8 8 8 8 1.41-1.41L7.83 13H20v-2z"/>
   </svg>
 )
+
+export const ChevronIcon: React.FC = () => (
+  <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="var(--icon-color)">
+    <path d="M0 0h24v24H0z" fill="none"/>
+    <path d="M10 6L8.59 7.41 13.17 12l-4.58 4.59L10 18l6-6z"/>
+  </svg>
+)
+
