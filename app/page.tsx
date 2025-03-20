a018b5771f2fee48779145eb205faa754694e666
tenmonster <tenmonster@hotmail.com>
2025-03-20 13:22:59 +0700

    Edit Mainpage

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,25 @@
-import styles from './page.module.css'
+"use client";
+
+import { useState } from "react";
+import styles from "./page.module.css";
+import Popup from "./components/popup";
 
 export default function Home() {
+  const [showPopup, setShowPopup] = useState(false);
+
   return (
-    <div className={styles.container}>
-        <h1>
-          Unit.co.Ltd Frontend Project
-        </h1>
+    <div className={styles.page}>
+      <div className={styles.containner}>
+        <h1>Unit.co.Ltd Frontend Project</h1>
+        <div className={styles.buttonlayout}>
+          <button 
+            className={styles.buttonpage}
+            onClick={() => setShowPopup(true)}>Click</button>
+          <p>Click to connect your phone</p>
+        </div>
+      </div>
+
+      {showPopup && <Popup onClose={() => setShowPopup(false)} />}
     </div>
-  )
+  );
 }
