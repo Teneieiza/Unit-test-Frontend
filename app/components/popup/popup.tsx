83edb700e6a02f156f456e647265c4600b848866
tenmonster <tenmonster@hotmail.com>
2025-03-21 22:46:29 +0700

    Add Iframe path(sort)&Eslint format

--- app/components/popup/popup.tsx
+++ app/components/popup/popup.tsx
@@ -1,41 +1,50 @@
-"use client";
+'use client'
 
-import styles from "./popup.module.css";
-import { useEffect } from "react";
-import { ChevronLeftIcon, ArrowIcon } from "../../icon/icon";
+import styles from './popup.module.css'
+import { useEffect } from 'react'
+import { ChevronLeftIcon, ArrowIcon } from '../../icon/icon'
 
 interface PopupProps {
-  onClose: () => void;
-  iframeSelect: string;
-  setIframeSelect: (select: string) => void;
+  onClose: () => void
+  iframeSelect: string
+  setIframeSelect: (select: string) => void
 }
 
-export default function Popup({ onClose, iframeSelect, setIframeSelect }: PopupProps) {
+export default function Popup({
+  onClose,
+  iframeSelect,
+  setIframeSelect,
+}: PopupProps) {
   useEffect(() => {
-    document.body.style.overflow = "hidden";
+    document.body.style.overflow = 'hidden'
     return () => {
-      document.body.style.overflow = "auto";
-    };
-  }, []);
+      document.body.style.overflow = 'auto'
+    }
+  }, [])
 
   const backPage = () => {
-    if (iframeSelect === "/pin") {
-      setIframeSelect("/phone");
+    if (iframeSelect === '/sort') {
+      setIframeSelect('/pin')
+    } else if (iframeSelect === '/pin') {
+      setIframeSelect('/phone')
     } else {
-      onClose();
+      onClose()
     }
-  };
+  }
 
   return (
     <div className={styles.overlay} onClick={onClose}>
       <div className={styles.popup} onClick={(e) => e.stopPropagation()}>
         <iframe src={iframeSelect} className={styles.iframe}></iframe>
-          <button 
-          onClick={backPage} 
-          className={`${styles.closeButton} ${iframeSelect === "/pin" ? styles.transparent : ""}`}>
-            {iframeSelect === "/pin" ? <ChevronLeftIcon /> : <ArrowIcon />}
-          </button>
+        <button
+          onClick={backPage}
+          className={`${styles.closeButton} ${
+            iframeSelect === '/pin' ? styles.transparent : ''
+          }`}
+        >
+          {iframeSelect === '/pin' ? <ChevronLeftIcon /> : <ArrowIcon />}
+        </button>
       </div>
     </div>
-  );
+  )
 }
