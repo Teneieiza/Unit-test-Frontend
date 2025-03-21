4e7ea3c062efc49f9cb97abe41cd2f0f6f57cf01
tenmonster <tenmonster@hotmail.com>
2025-03-21 22:49:36 +0700

    Edit Phonepage for recieve Phonenumber&Eslint format

--- app/phone/page.tsx
+++ app/phone/page.tsx
@@ -1,21 +1,17 @@
-"use client";
+'use client'
 
-import Form from "../components/form/form"
-import styles from "./phone.module.css"
+import Form from '../components/form/form'
+import styles from './phone.module.css'
 
 export default function PhonePage() {
-
-  const formSubmit = (event: React.FormEvent) => {
-    event.preventDefault();
-    window.parent.postMessage({ type: "SET_IFRAME", url: "/pin" }, "*");
+  const formSubmit = (phoneNumber: string) => {
+    window.parent.postMessage({ type: 'SET_IFRAME', url: '/pin' }, '*')
   }
   return (
     <div className={styles.page}>
       <div className={styles.container}>
         <div className={styles.header}>
-          <p>
-            เชื่อมต่อ
-          </p>
+          <p>เชื่อมต่อ</p>
           <p>ด้วยหมายเลขโทรศัพท์มือถือ</p>
         </div>
         <Form formSubmit={formSubmit} />
