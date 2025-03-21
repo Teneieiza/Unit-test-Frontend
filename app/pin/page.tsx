8ce1e66a3b29032a4337df837c92381793e45373
tenmonster <tenmonster@hotmail.com>
2025-03-21 18:58:27 +0700

    Edit Pin page&css

--- app/pin/page.tsx
+++ app/pin/page.tsx
@@ -1,9 +1,54 @@
+import Image from "next/image"
+import { BackSpace, ChevronRightIcon, FillCiecle } from "../icon/icon"
+import styles from "./pin.module.css"
+
 export default function PinPage() {
   return (
-    <div>
-        <h1>
-          This is Pin Page
-        </h1>
+    <div className={styles.page}>
+      <div className={styles.container}>
+        <div className={styles.header}>
+          <div>
+            <Image
+            src="/spinning-spining-cat.gif"
+            alt="UiaCat"
+            width={60}
+            height={60}
+            unoptimized
+            className={styles.imagecontent}
+            />
+          </div>
+          <div className={styles.pincontent}>
+            <p>ใส่ PIN เพื่อดำเนินการต่อ</p>
+            <div className={styles.pin}>
+              <FillCiecle/>
+              <FillCiecle/>
+              <FillCiecle/>
+              <FillCiecle/>
+              <FillCiecle/>
+              <FillCiecle/>
+            </div>
+          </div>
+          <div className={styles.forgotpass}>
+            <a>ลืมรหัสผ่าน <ChevronRightIcon fill="var(--forgot-pass-content)"/></a>
+          </div>
+        </div>
+        <div className={styles.footer}>
+          <button>1</button>
+          <button>2</button>
+          <button>3</button>
+          <button>4</button>
+          <button>5</button>
+          <button>6</button>
+          <button>7</button>
+          <button>8</button>
+          <button>9</button>
+          <div> </div>
+          <button>0</button>
+          <button>
+            <BackSpace/>
+          </button>
+        </div>
+      </div>
     </div>
   )
 }
