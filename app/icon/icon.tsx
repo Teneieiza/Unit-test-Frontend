7cfdf8b7f7153284574ee9b7113182b9f10f2a9e
tenmonster <tenmonster@hotmail.com>
2025-03-21 18:52:07 +0700

    Add more icon

--- app/icon/icon.tsx
+++ app/icon/icon.tsx
@@ -1,16 +1,67 @@
-import React from 'react';
+import React from 'react'
 
 export const ArrowIcon: React.FC = () => (
-  <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="var(--icon-color)">
-    <path d="M0 0h24v24H0z" fill="none"/>
-    <path d="M20 11H7.83l5.59-5.59L12 4l-8 8 8 8 1.41-1.41L7.83 13H20v-2z"/>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    height="24px"
+    viewBox="0 0 24 24"
+    width="24px"
+    fill="var(--icon-color)"
+  >
+    <path d="M0 0h24v24H0z" fill="none" />
+    <path d="M20 11H7.83l5.59-5.59L12 4l-8 8 8 8 1.41-1.41L7.83 13H20v-2z" />
   </svg>
 )
 
-export const ChevronIcon: React.FC = () => (
-  <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="var(--icon-color)">
-    <path d="M0 0h24v24H0z" fill="none"/>
-    <path d="M10 6L8.59 7.41 13.17 12l-4.58 4.59L10 18l6-6z"/>
+export const ChevronRightIcon: React.FC<React.SVGProps<SVGSVGElement>> = (props) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    height="24px"
+    viewBox="0 0 24 24"
+    width="24px"
+    fill={props.fill || "var(--icon-color)"}
+    {...props}
+  >
+    <path d="M0 0h24v24H0z" fill="none" />
+    <path d="M10 6L8.59 7.41 13.17 12l-4.58 4.59L10 18l6-6z" />
   </svg>
 )
 
+export const ChevronLeftIcon: React.FC = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    height="24px"
+    viewBox="0 0 24 24"
+    width="24px"
+    fill="var(--icon-color)"
+  >
+    <path d="M0 0h24v24H0V0z" fill="none" />
+    <path d="M15.41 16.59L10.83 12l4.58-4.59L14 6l-6 6 6 6 1.41-1.41z" />
+  </svg>
+)
+
+export const BackSpace: React.FC = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    height="24px"
+    viewBox="0 0 24 24"
+    width="24px"
+    fill="var(--text-pin)"
+  >
+    <path d="M0 0h24v24H0z" fill="none" />
+    <path d="M22 3H7c-.69 0-1.23.35-1.59.88L0 12l5.41 8.11c.36.53.9.89 1.59.89h15c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2zm-3 12.59L17.59 17 14 13.41 10.41 17 9 15.59 12.59 12 9 8.41 10.41 7 14 10.59 17.59 7 19 8.41 15.41 12 19 15.59z" />
+  </svg>
+)
+
+export const FillCiecle: React.FC = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    height="24px"
+    viewBox="0 0 24 24"
+    width="24px"
+    fill="var(--circle-background-pin)"
+  >
+    <path d="M24 24H0V0h24v24z" fill="none" />
+    <circle cx="12" cy="12" r="8" />
+  </svg>
+)
