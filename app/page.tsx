c9d8fee72f1f86ebe3f9e8b641e048fa0c43786f
tenmonster <tenmonster@hotmail.com>
2025-03-21 22:44:06 +0700

    Use Button from thirdparty&Eslint format

--- app/page.tsx
+++ app/page.tsx
@@ -10,16 +10,16 @@ export default function Home() {
 
   useEffect(() => {
     const handleMessage = (event: MessageEvent) => {
-      if (event.data?.type === "SET_IFRAME") {
-        setIframeSelect(event.data.url);
+      if (event.data?.type === 'SET_IFRAME') {
+        setIframeSelect(event.data.url)
       }
-    };
+    }
 
-    window.addEventListener("message", handleMessage);
+    window.addEventListener('message', handleMessage)
     return () => {
-      window.removeEventListener("message", handleMessage);
-    };
-  }, []);
+      window.removeEventListener('message', handleMessage)
+    }
+  }, [])
 
   return (
     <div className={styles.page}>
@@ -27,23 +27,27 @@ export default function Home() {
         <h1>Unit.co.Ltd Frontend Project</h1>
         <div className={styles.buttonlayout}>
           <button
-            className={styles.buttonpage}
+            className={styles.pushable}
+            role="button"
             onClick={() => {
-              setShowPopup(true);
-              setIframeSelect("/phone")
+              setShowPopup(true)
+              setIframeSelect('/phone')
             }}
           >
-            Click
+            <span className={styles.shadow}></span>
+            <span className={styles.edge}></span>
+            <span className={styles.front}>Click Me!!</span>
           </button>
+
           <p>Click to connect your phone</p>
         </div>
       </div>
 
       {showPopup && (
         <Popup
-        onClose={() => setShowPopup(false)}
-        iframeSelect={iframeSelect}
-        setIframeSelect={setIframeSelect}
+          onClose={() => setShowPopup(false)}
+          iframeSelect={iframeSelect}
+          setIframeSelect={setIframeSelect}
         />
       )}
     </div>
