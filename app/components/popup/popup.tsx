b2d660ccf1428e4464c3b603f3940ed9c7a61ccc
tenmonster <tenmonster@hotmail.com>
2025-03-21 18:54:01 +0700

    Change Popup folder&edit a little bit

---
+++ app/components/popup/popup.tsx
@@ -0,0 +1,41 @@
+"use client";
+
+import styles from "./popup.module.css";
+import { useEffect } from "react";
+import { ChevronLeftIcon, ArrowIcon } from "../../icon/icon";
+
+interface PopupProps {
+  onClose: () => void;
+  iframeSelect: string;
+  setIframeSelect: (select: string) => void;
+}
+
+export default function Popup({ onClose, iframeSelect, setIframeSelect }: PopupProps) {
+  useEffect(() => {
+    document.body.style.overflow = "hidden";
+    return () => {
+      document.body.style.overflow = "auto";
+    };
+  }, []);
+
+  const backPage = () => {
+    if (iframeSelect === "/pin") {
+      setIframeSelect("/phone");
+    } else {
+      onClose();
+    }
+  };
+
+  return (
+    <div className={styles.overlay} onClick={onClose}>
+      <div className={styles.popup} onClick={(e) => e.stopPropagation()}>
+        <iframe src={iframeSelect} className={styles.iframe}></iframe>
+          <button 
+          onClick={backPage} 
+          className={`${styles.closeButton} ${iframeSelect === "/pin" ? styles.transparent : ""}`}>
+            {iframeSelect === "/pin" ? <ChevronLeftIcon /> : <ArrowIcon />}
+          </button>
+      </div>
+    </div>
+  );
+}
