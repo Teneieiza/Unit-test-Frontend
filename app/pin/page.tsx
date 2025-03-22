2e14618e4cf2775924f5431571d6155b1a8d58c0
tenmonster <tenmonster@hotmail.com>
2025-03-22 15:43:11 +0700

    Add Pin page has use data&Set count of incorrect entrie

--- app/pin/page.tsx
+++ app/pin/page.tsx
@@ -4,23 +4,46 @@ import Image from 'next/image'
 import { BackSpace, ChevronRightIcon, FillCiecle } from '../icon/icon'
 import styles from './pin.module.css'
 import { useEffect, useState } from 'react'
+import { members } from '../data/member'
 
 export default function PinPage() {
   const pinLength = 6
-  const correctPin = '147369'
   const [pin, setPin] = useState<string>('')
   const [error, setError] = useState('')
+  const [count, setCount] = useState<number>(0)
+  const [lock, setLock] = useState<boolean>(false)
+  const [phoneNumber, setPhoneNumber] = useState<string | null>(null)
+
+  useEffect(() => {
+    const recievePhone = sessionStorage.getItem('phoneNumber')
+    if (recievePhone) {
+      setPhoneNumber(recievePhone)
+    }
+  }, [])
 
   useEffect(() => {
     if (pin.length === pinLength) {
-      if (pin === correctPin) {
+      const member = members.find((data) => data.phone === phoneNumber)
+      if (member && pin === member.pin) {
         window.parent.postMessage({ type: 'SET_IFRAME', url: '/sort' }, '*')
+        setCount(0)
       } else {
         setError('*PIN ไม่ถูกต้อง ลองใหม่อีกครั้ง!')
+        setCount((count) => count + 1)
         setPin('')
       }
+
+      if (count + 1 >= 3) {
+        setError('*กรุณาลองใหม่อีกครั้งใน 30 วินาที')
+        setLock(true)
+        setTimeout(() => {
+          setCount(0)
+          setLock(false)
+          setError('')
+        }, 30000)
+      }
     }
-  }, [pin])
+  }, [pin, phoneNumber, count])
 
   const handleNumberClick = (num: number) => {
     if (pin.length < pinLength) {
@@ -70,13 +93,28 @@ export default function PinPage() {
         </div>
         <div className={styles.footer}>
           {[...Array(9)].map((_, i) => (
-            <button key={i + 1} onClick={() => handleNumberClick(i + 1)}>
+            <button
+              key={i + 1}
+              onClick={() => handleNumberClick(i + 1)}
+              disabled={lock}
+              className={lock ? styles.lockbutton : ''}
+            >
               {i + 1}
             </button>
           ))}
           <div> </div>
-          <button onClick={() => handleNumberClick(0)}>0</button>
-          <button onClick={handleBackspace}>
+          <button
+            onClick={() => handleNumberClick(0)}
+            disabled={lock}
+            className={lock ? styles.lockbutton : ''}
+          >
+            0
+          </button>
+          <button
+            onClick={handleBackspace}
+            disabled={lock}
+            className={lock ? styles.lockbutton : ''}
+          >
             <BackSpace />
           </button>
         </div>
