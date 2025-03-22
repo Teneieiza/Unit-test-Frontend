67da7d055a6dc74af415b1bae62575b4e922bc92
tenmonster <tenmonster@hotmail.com>
2025-03-22 18:51:17 +0700

    Edit Func Format&Sort

--- app/sort/page.tsx
+++ app/sort/page.tsx
@@ -5,25 +5,26 @@ import styles from './sort.module.css'
 import CodeBox from '../components/codebox/codebox'
 
 export default function page() {
-  const originalNumbers: number[] = [8, 9, 1, 7, 3, 0, 2, 5, 4, 6]
+  const originalNumbers: number = 8917302546
   const [sortedNumbers, setSortedNumbers] = useState<number[]>([])
 
   const handleSort = () => {
-    setSortedNumbers([...originalNumbers].sort((a, b) => a - b))
+    const numberToArray = Array.from(String(originalNumbers), Number)
+    setSortedNumbers([...numberToArray].sort((a, b) => a - b))
   }
 
   const code = `
   ขั้นแรกประกาศตัวแปร และ Usestate มาเพื่อรับค่า
   ของตัวเลขและตัวเลขที่เรียงแล้ว
-  const originalNumbers: number[]
-  = [8, 9, 1, 7, 3, 0, 2, 5, 4, 6]
+  const originalNumbers: number = 8917302546
 
   const [sortedNumbers, setSortedNumbers]
   = useState<number[]>([])
 
-
-  จากนั้นสร้าง func สำหรับ sort ข้อมูลและเรียกใช้งาน      
+  จากนั้นสร้าง func สำหรับแปลงตัวเลขเป็น Array&Sort ข้อมูล 
   const handleSort = () => {
+    const numberToArray = Array
+    .from(String(originalNumbers), Number)
     setSortedNumbers(
       [...originalNumbers].sort((a, b) => a - b)
     )
@@ -35,7 +36,7 @@ export default function page() {
       <section className={styles.section}>
         <div className={styles.topsection}>
           <h1>
-            ตัวเลขเริ่มต้น: <span>{originalNumbers.join(' ')}</span>
+            ตัวเลขเริ่มต้น: <span>{originalNumbers.toString().split('').join(' ')}</span>
           </h1>
           <button onClick={handleSort}> click to sort</button>
         </div>
