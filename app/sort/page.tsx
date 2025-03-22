391b25576f39ec3d662aba34070698d2aec43d2a
tenmonster <tenmonster@hotmail.com>
2025-03-22 14:37:15 +0700

    Edit Sort page&CSS

--- app/sort/page.tsx
+++ app/sort/page.tsx
@@ -1,7 +1,55 @@
+'use client'
+
+import { useState } from 'react'
+import styles from './sort.module.css'
+import CodeBox from '../components/codebox/codebox'
+
 export default function page() {
+  const originalNumbers: number[] = [8, 9, 1, 7, 3, 0, 2, 5, 4, 6]
+  const [sortedNumbers, setSortedNumbers] = useState<number[]>([])
+
+  const handleSort = () => {
+    setSortedNumbers([...originalNumbers].sort((a, b) => a - b))
+  }
+
+  const code = `
+  ขั้นแรกประกาศตัวแปร และ Usestate มาเพื่อรับค่า
+  ของตัวเลขและตัวเลขที่เรียงแล้ว
+  const originalNumbers: number[]
+  = [8, 9, 1, 7, 3, 0, 2, 5, 4, 6]
+
+  const [sortedNumbers, setSortedNumbers]
+  = useState<number[]>([])
+
+
+  จากนั้นสร้าง func สำหรับ sort ข้อมูลและเรียกใช้งาน      
+  const handleSort = () => {
+    setSortedNumbers(
+      [...originalNumbers].sort((a, b) => a - b)
+    )
+  }
+  `
+
   return (
-    <div>
-      This is Sort page
+    <div className={styles.page}>
+      <section className={styles.section}>
+        <div className={styles.topsection}>
+          <h1>
+            ตัวเลขเริ่มต้น: <span>{originalNumbers.join(' ')}</span>
+          </h1>
+          <button onClick={handleSort}> click to sort</button>
+        </div>
+        <div className={styles.btmsection}>
+          {sortedNumbers.length > 0 && (
+            <h2>
+              ตัวเลขที่เรียงแล้ว: <span>{sortedNumbers.join(' ')}</span>
+            </h2>
+          )}
+        </div>
+        <div className={styles.codesection}>
+          <CodeBox code={code} language="tsx"/>
+        </div>
+      </section>
     </div>
-  );
+  )
 }
